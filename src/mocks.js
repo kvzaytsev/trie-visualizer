7c2e292263ffdc56f1b50b8d26fb99c473f47c1f
Kirill Zaytsev <kzaytsev@MBP-Kirill.petersburg.epam.com>
2018-03-15 13:05:46 +0300

    removed text

--- src/mocks.js
+++ src/mocks.js
@@ -1,2 +1,2 @@
-export const textMock = "cabbage cabby cabdriver cabdriving cactus cackle coach coaching beacon beach beachcomber beard bearish bitch bitcoin time timber timbrel tinsel weak weapon wearied wears weasel" ;
+export const textMock = "cabbage cabby cabdriver cabdriving cactus cackle coach coaching beacon beach beachcomber beard bearish bitcoin time timber timbrel tinsel weak weapon wearied wears weasel" ;
 export const textMock1 = "ca cb cd";
